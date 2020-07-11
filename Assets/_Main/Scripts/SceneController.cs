4cc56d2b87c0bb614d28994a340bc2bdef50b709
januarelsan <januarelsan@gmail.com>
2020-07-11 23:28:30 +0700

    Init Project and Develop Flashcard

---
+++ Assets/_Main/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : Singleton<SceneController> {
+
+	void Awake(){
+		
+		
+	}
+
+	public void restartScene (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	public void goToScene(string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void quitGame(){		
+		Application.Quit ();
+	}
+
+	public string getSceneLoaded(){
+		return Application.loadedLevelName;
+	}
+		
+}
---
+++ Assets/_Main/TouchEffect/TouchEffectManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEffectManager : MonoBehaviour
+{
+
+    
+    [SerializeField]
+    private GameObject touchEffect = null;
+    [SerializeField]
+    private AudioClip touchClip = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            
+            Vector3 pos = Input.mousePosition;            
+            Vector3 realWorldPos = Camera.main.ScreenToWorldPoint(pos);
+
+            GameObject tEffect = Instantiate(touchEffect, realWorldPos, this.transform.rotation, this.transform);
+            GetComponent<AudioSource>().PlayOneShot(touchClip);
+        }
+        
+    }
+
+    
+}
