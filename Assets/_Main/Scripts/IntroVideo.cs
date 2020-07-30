f75340493d117fbc8ba293325391bbf5271e86d0
januarelsan <januarelsan@gmail.com>
2020-07-31 01:33:48 +0700

    Add Intro Video

---
+++ Assets/_Main/Scripts/IntroVideo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class IntroVideo : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        // Each time we reach the end, we slow down the playback by a factor of 10.
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+        SceneController.Instance.goToScene("Home");
+    }
+}
