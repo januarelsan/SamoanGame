ca7b15bbe2da7188acad6506dfa0b8214174f36e
januarelsan <januarelsan@gmail.com>
2020-08-16 06:36:36 +0700

    Update Card

---
+++ Assets/_Main/Scripts/RateApp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RateApp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowDialog(){
+        Application.OpenURL ("market://details?id=" + "com.littlelearnerssamoa");
+    }
+}
